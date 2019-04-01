76ba5ec60af7a9cf991363525b99e64051ee9d74
eshin21 <eshin135@gmail.com>
2019-03-31 22:26:20 -0400

    Setting up ship class

---
+++ Ship.java
@@ -0,0 +1,16 @@
+public class Ship{
+	
+	int x;
+	int y;
+	String shape;
+	int health;
+	public Ship(int x, int y,  String shape, int health){
+		this.x = x;
+		this.y = y;
+		this.shape = shape;
+		this.health =health;
+		
+	}
+	
+	
+}
