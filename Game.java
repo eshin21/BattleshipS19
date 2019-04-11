d90f6fd898e4d0d15a0b816209545620b3c3cf01
eshin21 <eshin21@vlan502-49.subnet-191.amherst.edu>
2019-04-10 20:59:53 -0400

    game changes

--- Game.java
+++ Game.java
@@ -1,3 +1,49 @@
-public class Game{
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
+public class Game{
+	
+	private Ship[] grid;
+	private int moveCount;
+	public int shipsPlacedA; //keeps track of whether player A has placed all ships
+	public int shipsPlacedB; //keeps track of whether player B has placed all ships
+	public boolean gameOver; //need to keep track of when game stops
+	
+	public Game() {
+		
+		this.grid = new Ship[];
+		this.moveCount = 0;
+		
+	}
+	
+	public void play() {
+		
+		
+		if(moveCount % 2 == 0) {
+			System.out.println("It's Player 1's turn: pass the computer.");
+			
+		}
+		
+		else
+			System.out.println("It's Player 2's turn: pass the computer.");
+		
+		
+		if(moveCount <= 1) {
+			System.out.println("Place your ships.")
+			
+			while(shipsPlaced == false) {
+				
+				
+				
+			}
+
+			
+		}
+	
+		
+		
+	}
+	
 }
