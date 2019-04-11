ff299c6472f45eeacb8ea628b61ca2c20377648a
Claire Cho <clairecho@vlan413-191.subnet-252.amherst.edu>
2019-04-11 14:41:19 -0400

    Nothing changed, just spacing.

--- Game.java
+++ Game.java
@@ -27,24 +27,8 @@ public class Game{
 	}
 
 	public void play() {
-<<<<<<< HEAD
 
 
-=======
-		
-		Pair begin = new Pair(0,0);
-
-		
-		world.armada_A = new Ship[5];
-		world.armada_B = new Ship[5];
-		
-		for(int i = 0; i<=4; ++i) {
-			world.armada_A[i] = new Ship(begin, 2+i, 2+i, Color.BLUE, 0);
-			world.armada_B[i] = new Ship(begin, 2+i, 2+i, Color.RED, 0);
-	
-		}
-		
->>>>>>> 9099df196bd555612231859c339cb332a0b3e370
 		while(moveCount <= 1 & armada_A.length() != 5 & armada_B.length() != 5) {
 			placeShip();
 
@@ -91,14 +75,10 @@ public class Game{
 
 
 	public void placeShip() {
-<<<<<<< HEAD
 
 		MouseInput userIn = new MouseInput();
 
 		if(mouse)
-=======
-				
->>>>>>> 9099df196bd555612231859c339cb332a0b3e370
 		//left off here
 
 	}
@@ -111,12 +91,8 @@ class MouseShips extends JPanel implements MouseListener{
     	public static final int WIDTH = 1024;
     	public static final int HEIGHT = 768;
     	public static final int FPS = 60;
-<<<<<<< HEAD
     	World world;
 
-=======
-    
->>>>>>> 9099df196bd555612231859c339cb332a0b3e370
     class Runner implements Runnable{
     		public void run(){
     			while(true){
