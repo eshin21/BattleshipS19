0f2c06d01e97a2e8f3dcb21c2270339fd583ac2d
Claire Cho <clairecho@vlan413-191.subnet-252.amherst.edu>
2019-04-14 16:12:17 -0400

    Changed test method

--- test.java
+++ test.java
@@ -4,26 +4,28 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.util.Random;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
-public class Test extends JPanel{
+
+public class Test extends JPanel implements MouseListener{
 	public static final int BOX_WIDTH = 1024;
 	public static final int BOX_HEIGHT = 768;
-	public Pair[][] positionGrid;
+	public CoordinateRange[][] positionGrid;
+	public static Pair point;
 
 	public Test(){
 			this.setPreferredSize(new Dimension(BOX_WIDTH, BOX_HEIGHT));
 	}
 
 
-	public static void main (String[] args){
+public static void main (String[] args){
 		JFrame frame = new JFrame("Test Window");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(new Test());
 		frame.pack();
 		frame.setVisible(true);
 	}
+
 
 
 	public void draw(Graphics g, int startX, int startY){
@@ -34,6 +36,8 @@ public class Test extends JPanel{
 			int columnX = startX;
 			int columnY = startY;
 
+			positionGrid = new CoordinateRange[row][column];
+			fillPositionGrid(startX,startY,row,column);
 
 			for (int r=0; r<=row; r++){
 				g.setColor(Color.WHITE);
@@ -55,6 +59,69 @@ public class Test extends JPanel{
 
 		}
 
+		public void fillPositionGrid(int startX, int startY, int row, int column){
+			double beginX = startX;
+			double beginY = startY;
+			double endX = startX+50;
+			double endY = startY+50;
+			Pair xcoord = new Pair(beginX,endX);
+			Pair ycoord = new Pair(beginY,endY);
+			CoordinateRange range = new CoordinateRange(xcoord,ycoord);
+
+			for (int i=0;i<row;i++){
+				for (int j=0;j<column;j++){
+					System.out.println(range);
+					positionGrid[i][j] = range;
+					beginX += 50;
+					endX = beginX + 50;
+					range.xcoord = new Pair(beginX,endX);
+				}
+				beginX = startX;
+				endX = beginX + 50;
+				range.xcoord = new Pair(beginX,endX);
+				beginY += 50;
+				endY = beginY + 50;
+				range.ycoord = new Pair(beginY,endY);
+			}
+
+		}
+
+	@Override
+	 public void mouseClicked(MouseEvent e) {
+
+	  if(e.getButton() == 1){
+
+		 	this.point = new Pair(e.getX(), e.getY());
+		  System.out.println("You clicked "  + this.point);
+	   	this.repaint();
+	   // JOptionPane.showMessageDialog(null,e.getX()+ "\n" + e.getY());
+	  }
+
+	 }
+
+	 @Override
+	 	public void mouseEntered(MouseEvent e){
+	  // TODO Auto-generated method stub
+
+	 }
+
+	 @Override
+	 public void mouseExited(MouseEvent e) {
+	  // TODO Auto-generated method stub
+
+	 }
+
+	 @Override
+	 public void mousePressed(MouseEvent e) {
+	  // TODO Auto-generated method stub
+
+	 }
+
+	 @Override
+	 public void mouseReleased(MouseEvent e) {
+	  // TODO Auto-generated method stub
+
+	 }
 
 	@Override
 	public void paintComponent(Graphics g) {
