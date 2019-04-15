3a77a1f0446d62addc9c7b64d7017be4175a29e5
Claire Cho <clairecho@vlan413-191.subnet-252.amherst.edu>
2019-04-15 09:18:39 -0400

    changed xrange/yrange to fit coordinateRange member variable names.

--- Test.java
+++ Test.java
@@ -20,7 +20,7 @@ public class Test extends JPanel implements MouseListener{
 	public static CoordinateRange[][] GridB;
 	public static Pair point;
 	public ArrayList<Rectangle> rects = new ArrayList<Rectangle>();
-	
+
 
 	public Test(){
 			this.setPreferredSize(new Dimension(BOX_WIDTH, BOX_HEIGHT));
@@ -39,37 +39,37 @@ public static void main (String[] args){
 /////////SET POSITION GRIDS FOR LOCKING//////////////////////
 
 public void setPositions() {
-	
+
 	GridA = new CoordinateRange[10][10];
-	GridB = new CoordinateRange [10][10];
-	
+	GridB = new CoordinateRange[10][10];
+
 	for(int i = 0; i< 10; i++) {
-		
+
 		for(int j = 0; j<10; ++j) {
-			
+
 			Pair xrange = new Pair(25+45*i, 70+45*i);
-			
+
 			Pair yrange = new Pair(25+45*j, 70+45*j);
 			GridA[i][j] = new CoordinateRange(xrange, yrange);
-			
+
 		}
-		
+
 	}
-	
+
 	for(int i = 0; i< 10; i++) {
-		
+
 		for(int j = 0; j<10; ++j) {
-			
+
 			Pair xrange = new Pair(520+45*i, 565+45*i);
-			
+
 			Pair yrange = new Pair(25+45*j, 70+45*j);
-			GridB[i][j] = new CoordinateRange(xrange, yrange);
-			
+			GridB[i][j] = new CoordinateRange(xrange,yrange);
+
 		}
-		
+
 	}
-	
-	
+
+
 }
 
 public void draw(Graphics g, int startX, int startY){
@@ -83,7 +83,7 @@ public void draw(Graphics g, int startX, int startY){
 			this.addMouseListener(this);
 
 			this.setPositions(); /////CALL SETPOSITIONS METHOD (generates the positions of anchor points to lock in user selections to a discrete square/////
-			
+
 
 			for (int r=0; r<=row; r++){ ////draw grid
 				g.setColor(Color.WHITE);
@@ -104,11 +104,11 @@ public void draw(Graphics g, int startX, int startY){
 		}
 
 	public Pair findPoint (Pair point){ ////takes user click and decides which square this clicked point belongs to ("locking in" user input)
-		
+
 			double x = point.x;
 			double y = point.y;
 			Pair cornerPoint = new Pair(25,25);
-			
+
 			if(x <= 475 && y <=475) ///if click was in Grid A, call findPointA
 				cornerPoint = findPointA(point);
 			else if(x>=520 && y<=475)///if click was in Grid B, call findPointB
@@ -117,74 +117,73 @@ public void draw(Graphics g, int startX, int startY){
 			return cornerPoint;
 
 	}
-	
-	
-	///FIND ANCHOR POINT BASED ON USER CLICK IN GRID A 
-	
-	public Pair findPointA (Pair point) { 
-		
-		
-		double minX = GridA[0][0].xrange.x; ///INITIALIZATION 
-		double maxX = GridA[0][0].xrange.y;
-		double minY = GridA[0][0].yrange.x;
-		double maxY = GridA[0][0].yrange.y;
-		
+
+
+	///FIND ANCHOR POINT BASED ON USER CLICK IN GRID A
+
+	public Pair findPointA (Pair point) {
+
+
+		double minX = GridA[0][0].xcoord.x; ///INITIALIZATION
+		double maxX = GridA[0][0].xcoord.y;
+		double minY = GridA[0][0].ycoord.x;
+		double maxY = GridA[0][0].ycoord.y;
+
 		Pair corner = new Pair(0,0);
-		
+
 			for(int i = 0; i<10; ++i) {
-				
+
 				for(int j = 0; j<10; ++j) {
-					minX = GridA[i][j].xrange.x;
-					maxX = GridA[i][j].xrange.y;
-					minY = GridA[i][j].yrange.x;
-					maxY = GridA[i][j].yrange.y;
+					minX = GridA[i][j].xcoord.x;
+					maxX = GridA[i][j].xcoord.y;
+					minY = GridA[i][j].ycoord.x;
+					maxY = GridA[i][j].ycoord.y;
 					if(point.x >= minX && point.x <= maxX && point.y >= minY && point.y <= maxY )
 						corner = new Pair(minX, minY);
-					
+
 				}
-				
+
 			}
-			
-			
+
 			return corner;
-		
-			
+
+
 }
 
-	///FIND ANCHOR POINT BASED ON USER CLICK IN GRID B 
-	
-	public Pair findPointB (Pair point) { 
-		
-		
+	///FIND ANCHOR POINT BASED ON USER CLICK IN GRID B
 
-		double minX = GridB[0][0].xrange.x;
-		double maxX = GridB[0][0].xrange.y;
-		double minY = GridB[0][0].yrange.x;
-		double maxY = GridB[0][0].yrange.y;
-	
-	
+	public Pair findPointB (Pair point) {
+
+
+
+		double minX = GridB[0][0].xcoord.x;
+		double maxX = GridB[0][0].xcoord.y;
+		double minY = GridB[0][0].ycoord.x;
+		double maxY = GridB[0][0].ycoord.y;
+
+
 		Pair corner = new Pair(0,0);
-		
+
 			for(int i = 0; i<10; ++i) {
-				
-				for(int j = 0; j<10; ++j) {	
-					
-					minX = GridB[i][j].xrange.x;
-					maxX = GridB[i][j].xrange.y;
-					minY = GridB[i][j].yrange.x;
-					maxY = GridB[i][j].yrange.y;
+
+				for(int j = 0; j<10; ++j) {
+
+					minX = GridB[i][j].xcoord.x;
+					maxX = GridB[i][j].xcoord.y;
+					minY = GridB[i][j].ycoord.x;
+					maxY = GridB[i][j].ycoord.y;
 					if(point.x >= minX && point.x <= maxX && point.y >= minY && point.y <= maxY )
 						corner = new Pair(minX, minY);
-					
+
 				}
-				
-				
+
+
 			}
-			
-			
+
+
 			return corner;
-		
-			
+
+
 }
 
 
@@ -196,9 +195,9 @@ public void draw(Graphics g, int startX, int startY){
 		 this.point = new Pair(e.getX(), e.getY());
 		 System.out.println("You clicked "  + this.point);
 		 this.repaint();
-		
+
 	  }
-		 
+
 	  }
 
 ///////////IGNORE THESE : NECESSARY FOR COMPILING
@@ -226,32 +225,32 @@ public void draw(Graphics g, int startX, int startY){
 	  // TODO Auto-generated method stub
 
 	 }
-	 
+
 /////////////////////////////END IGNORE
-	 
-	 
-	 
+
+
+
 //WAS TOLD FROM https://stackoverflow.com/questions/13549506/drawing-a-shape-but-keeping-the-most-previous-shape-on-screen-in-java THAT UPDATE METHOD COULD BE USED FOR PRESERVING SQUARES WHEN REPAINTING, BUT THIS WAS UNNECESS.
 //	@Override
 //	public void update(Graphics g) {
-//		
+//
 //		paint(g);
-//		
+//
 //	}
-	 
+
 
 	@Override
 	public void paintComponent(Graphics g) {
-		
+
 		super.paintComponent(g);
 		g.setColor(Color.BLACK); // sets background color
 		g.fillRect(0, 0, BOX_WIDTH, BOX_HEIGHT);
 
 		draw(g,25,25);
 		draw(g,520,25);
-		 
+
 		if(rects != null) { //NEED TO MAKE ARRAYLIST OF RECTANGLES SO REPAINT DOESN'T DELETE THEM EVERY TIME WE DRAW A NEW RECTANGLE
-			for(Rectangle r : rects) {	
+			for(Rectangle r : rects) {
 				g.setColor(r.color);
 				g.fillRect(r.x, r.y, 45, 45);
 			}
@@ -261,12 +260,12 @@ public void draw(Graphics g, int startX, int startY){
 
 
 			g.setColor(Color.RED); //set color
-			Pair corner = this.findPoint(this.point); //find corner 
-			System.out.println("The key corner is " + corner); 
+			Pair corner = this.findPoint(this.point); //find corner
+			System.out.println("The key corner is " + corner);
 			Rectangle add = new Rectangle((int)corner.x, (int)corner.y,45,45, Color.RED); //make new rectangle based on this corner
 			rects.add(add); //add to arraylist of rectangles
-			
-			
+
+
 			g.fillRect((int)corner.x, (int)corner.y, 45, 45); //somehow necessary--otherwise it won't draw until the next click
 
 		}
@@ -275,15 +274,15 @@ public void draw(Graphics g, int startX, int startY){
 
 
 			g.setColor(Color.GREEN);
-			
+
 			Pair corner = this.findPoint(this.point);
-			
+
 			System.out.println("The key corner is " + corner);
-			
+
 			Rectangle add = new Rectangle((int)corner.x, (int)corner.y,45,45, Color.GREEN);
 			rects.add(add);
-			
-			
+
+
 			g.fillRect((int)corner.x, (int)corner.y, 45, 45); //necessary
 
 		}
